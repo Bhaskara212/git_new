4e375439f045b8054a61986a69dadbdb2a055c26
teja <pitchikas.212@gmail.com>
2023-02-24 19:42:13 +0530

    New-conflict is resloved

--- demo4.java
+++ demo4.java
@@ -1,11 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
 	    // print statement is changed
-<<<<<<< HEAD
-	 System.out.println("modification done on local)
-=======
 	    System.out.println("modification done on demo2.java on remote");
->>>>>>> c12239c13b956e8ffc1761116331f82bd0a72ab8
         System.out.println("Hello, World!"); 
     }
 }
